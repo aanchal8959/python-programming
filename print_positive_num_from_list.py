1ab5156dc1b46173cd26cc830634b0aaff1863bc
Aanchal Gupta <82527522+aanchal8959@users.noreply.github.com>
2022-01-08 15:08:33 +0530

    created print_positive_num_from_list.py
    This file will print all the positive no. present in list and left the negative numbers.

---
+++ print_positive_num_from_list.py
@@ -0,0 +1,15 @@
+list1=[12,-7,5,64,-14]
+print("input: ",list1)
+print("output: ",end="")
+for number in list1:
+    if number>=0:
+        print(number,end=" ")
+print("\n")     
+    
+    
+list2=[12,14,-95,3]
+print("input: ",list2)
+print("output: " ,end="")
+for num in list2:
+        if num>=0:
+           print(num,end=" ")
