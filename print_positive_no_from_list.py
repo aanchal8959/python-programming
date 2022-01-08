609c4f1675cfb7ce8f1899d6e233f25761021f06
Aanchal Gupta <82527522+aanchal8959@users.noreply.github.com>
2022-01-08 17:13:37 +0530

    created print_positive_no_from_list.py
    This file print the list of positive no. from original list.

---
+++ print_positive_no_from_list.py
@@ -0,0 +1,18 @@
+list1=[12,-7,5,64,-14]
+print("input: ",list1)
+print("output: ",end="")
+for number in list1:
+    if number>=0:
+        print(number,end=",")
+print("\n")     
+    
+    
+list2=[12,14,-95,3]
+l2=[]
+print("input: ",list2)
+print("output: " ,end="")
+for num in list2:
+        if num>=0:
+            l2.append(num)
+print(l2,end=" ")
+
